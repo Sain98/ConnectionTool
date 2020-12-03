5113d17ba2785505261ef7c60fb8ff6dd7384f16
_Sain <sander.lantin@gmail.com>
2020-12-03 11:05:57 +0100

    Notification support

--- tool/NotificationHandler.py
+++ tool/NotificationHandler.py
@@ -1,3 +1,8 @@
-class NotificationHandler:
-    def __init__(self, logger):
-        pass
+from plyer import notification as notify
+
+def notification(old_status, new_status, server):
+    notify.notify(
+        title=f"{server} - Status change!",
+        message=f"Old status: {old_status} | New status: {new_status}",
+        app_name="ConnectionTool"
+    )
